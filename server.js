f4f5020d6ee1e39f5596a4d6850a24e493169a06
Bruno <bruno.umbelino0@gmail.com>
2021-09-13 22:22:20 -0300

    não sei oque eu fiz

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const Routes = require("./src/Routes");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(Routes);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(Routes);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
--- src/Controller.js
+++ src/Controller.js
@@ -1,12 +1,12 @@
-const { v4: uuidv4 } = require("uuid");
-
 const Users = [];
 
 const AddUser = (req, res) => {
+  const { v4: uuidv4 } = require("uuid");
+
   const _id = uuidv4().substr(0, 8);
   const { username } = req.body;
-  const newUser = { _id, username };
 
+  const newUser = { _id, username };
   Users.push(newUser);
 
   res.json(newUser);
